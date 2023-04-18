b2653f83a8d762ce514622a6f9a93b740bda733a
marcodigennaro <m.di.gennaro@outlook.com>
2023-04-18 11:31:18 +0200

    added requirements_dev.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(
-    name='common_stuff',
-    version="0.0.1",
-    description="common functions to be used for data analysis",
-    package_dir={"": "src"},
-    author="Marco Di Gennaro",
-    author_email="m.di.gennaro@outlook.com",
-    url="https://mdigennaro.space",
-    python_requires=">=3.7"
-)
+if __name__ == "__main__":
+    setup(
+        name='mylib',
+        version="0.0.1",
+        description="common functions to be used for data analysis",
+        package_dir={"": "src"},
+        author="Marco Di Gennaro",
+        author_email="m.di.gennaro@outlook.com",
+        url="https://mdigennaro.space",
+        python_requires=">=3.7"
+    )
+
--- src/mylib/functions.py
+++ src/mylib/functions.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 
-def unit_vector(vector):
-    """ Returns the unit vector of the vector.  """
-    return vector / np.linalg.norm(vector)
+def unit_vector(my_array: np.ndarray) -> np.ndarray:
+    """
+    Returns the unit vector of the vector.
+    - Input: a N-dim vector
+    - Output: scalar
+    - Example:
+        unit_vector([1,1,1]) = 1.7320508075688772
+    """
+    return my_array / np.linalg.norm(my_array)
 
 
-def angle_between(v1: np.array , v2: np.array ):
+def angle_between(v1: np.array , v2: np.array ) -> float:
     """
     Returns the angle in radians between vectors 'v1' and 'v2':
     - Inputs: two vectors
@@ -19,4 +25,6 @@ def angle_between(v1: np.array , v2: np.array ):
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
 
